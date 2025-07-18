434d3681d5a75ef9ef4e1438d8969f784667463f
satya-sri-karri <satyasrikarri457@gmail.com>
2025-07-18 06:48:37 +0530

    Add files via upload

---
+++ basic.h
@@ -0,0 +1,20 @@
+#ifndef basic_functions
+#define basic_functions
+#include<iostream>
+using namespace std;
+int add(int x,int y){
+	return x+y;
+}
+int subtract(int x,int y){
+	return x-y;
+}
+int divide(int x,int y){
+	return x/y;
+}
+int multiply(int x,int y){
+	return x*y;
+}
+int modulo(int x,int y){
+	return x%y;
+}
+#endif
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include "basic.h"
+using namespace std;
+int main(){
+	int a,b;
+	cin >> a >> b;
+	cout << add(a,b);
+}
